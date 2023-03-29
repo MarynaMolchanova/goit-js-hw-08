4f8b569b878e35bbe62a89297121b0726b38cd3b
MarynaMolchanova <108984910+MarynaMolchanova@users.noreply.github.com>
2023-03-29 23:48:58 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,21 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-const STORAGE_KEY = "videoplayer-current-time";
+const STORAGE_KEY = 'videoplayer-current-time';
 const currentTime = localStorage.getItem(STORAGE_KEY)
   ? localStorage.getItem(STORAGE_KEY)
   : 0;
 
-player.on("play", function () {
-  console.log("played the video!");
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
-player.getVideoTitle().then(function (title) {
-  console.log("title:", title);
-});
-
-const saveTimeVideo = (event) => {
+const saveTimeVideo = event => {
   localStorage.setItem(STORAGE_KEY, event.seconds);
 };
 
 player.setCurrentTime(currentTime);
 
-player.on("timeupdate", throttle(saveTimeVideo, 1000));
+player.on('timeupdate', throttle(saveTimeVideo, 1000));
